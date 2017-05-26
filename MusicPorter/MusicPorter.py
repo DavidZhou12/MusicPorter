dcc2e57b664a8c57aaa4b673e84bc55c63768331
David Zhou <DavidZhouToday@gmail.com>
2017-05-26 17:17:22 -0400

    Add function to get mp3 files in current directory

--- MusicPorter/MusicPorter.py
+++ MusicPorter/MusicPorter.py
@@ -1,14 +1,19 @@
 import os, sndhdr, sys
 import eyed3
 
-def checkMp3(path):
-    if path.lower().endswith('.mp3'):
+def isMp3(fileName):
+    if fileName.lower().endswith('.mp3'):
         return True
     else:
         return False
 
+def GetCurrentDirectoryMp3List():
+    mp3Files = [file for file in os.listdir() if isMp3(file)]
+    return mp3Files
+
 def main():
-    print("Hello World!")
+    mp3Files = GetCurrentDirectoryMp3List()
+    print(*mp3Files, sep='\n')
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
