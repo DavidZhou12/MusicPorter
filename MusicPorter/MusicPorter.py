7aebb1b06ee70a9fa25adbb03d94373de7956093
David Zhou <DavidZhouToday@gmail.com>
2017-05-28 16:51:43 -0400

    Fix naming convention

--- MusicPorter/MusicPorter.py
+++ MusicPorter/MusicPorter.py
@@ -20,108 +20,111 @@ from codecs import decode
 # (mp3FilePath, Title, Album, Artist, Track Number)
 #
 
-def printEditableList():
+# Prints editable EasyID3 tags
+def print_editable_list():
     print('\n'.join(EasyID3.valid_keys.keys()))
 
-def isMp3(fileName):
-    if fileName.lower().endswith('.mp3'):
+def is_mp3(file_name):
+    if file_name.lower().endswith('.mp3'):
         return True
     else:
         return False
 
-def GetCurrentDirectoryMp3List():
-    mp3Files = [file for file in os.listdir() if isMp3(file)]
-    return mp3Files
+def get_current_directory_mp3():
+    mp3_files = [file for file in os.listdir() if is_mp3(file)]
+    return mp3_files
 
 # Ensures every mp3 file in the list has an ID3 tag with: title, album, artist
 # Returns a sorted list of every mp3 file
-def getSortedList(mp3Files):
-    sortedList = []
-    for mp3FilePath in mp3Files:
+def get_sorted_list(mp3_files):
+    sorted_list = []
+    for mp3_file_path in mp3_files:
         try:                                    # Test if the file has an ID3 tag
-            audioFile = EasyID3(mp3FilePath)
+            audio_file = EasyID3(mp3_file_path)
 
             #Check if the three tags are in the ID3 file
             modified_flag = False
-            if "title" not in audioFile or "album" not in audioFile or "artist" not in audioFile:
+            if "title" not in audio_file or "album" not in audio_file or "artist" not in audio_file:
                 modified_flag = True
-            if "title" not in audioFile:
-                audioFile["title"] = [os.path.splitext(mp3FilePath)[0]]
-            if "album" not in audioFile:
-                audioFile["album"] = [u"Unknown Album"]
-            if "artist" not in audioFile:
-                audioFile["artist"] = [u"Unknown Artist"]
+            if "title" not in audio_file:
+                audio_file["title"] = [os.path.splitext(mp3_file_path)[0]]
+            if "album" not in audio_file:
+                audio_file["album"] = [u"Unknown Album"]
+            if "artist" not in audio_file:
+                audio_file["artist"] = [u"Unknown Artist"]
             if modified_flag == True:
-                audioFile.save(mp3FilePath, v1=2, v2_version = 3)
+                audio_file.save(mp3_file_path, v1=2, v2_version = 3)
 
         except mutagen.id3.ID3NoHeaderError:    # If no ID3 tag present, create one
-            audioFile = EasyID3()
-            audioFile["title"] = [os.path.splitext(mp3FilePath)[0]]
-            audioFile["album"] = [u"Unknown Album"]
-            audioFile["artist"] = [u"Unknown Artist"]
-            audioFile.save(mp3FilePath, v1=2, v2_version = 3)
-        sortedList.append((mp3FilePath, audioFile["title"][0], audioFile["album"][0], audioFile["artist"][0]))
+            audio_file = EasyID3()
+            audio_file["title"] = [os.path.splitext(mp3_file_path)[0]]
+            audio_file["album"] = [u"Unknown Album"]
+            audio_file["artist"] = [u"Unknown Artist"]
+            audio_file.save(mp3_file_path, v1=2, v2_version = 3)
+        sorted_list.append((mp3_file_path, audio_file["title"][0], audio_file["album"][0], audio_file["artist"][0]))
     
     # Multi-level sorting
-    sortedList = sorted(sortedList, key=itemgetter(2, 3, 1, 0))
+    sorted_list = sorted(sorted_list, key=itemgetter(2, 3, 1, 0))
         
     # Expect every item to have an ID3 tag at this point
     # Add track number to mp3 files based on the sorted list
-    sortedListWithTrackNumbers = []
-    trackNumber = 1
-    for item in sortedList:
-        audioFile = EasyID3(item[0])
-        audioFile["tracknumber"] = [str(trackNumber)]
-        audioFile.save(item[0], v1=2, v2_version = 3)
-        sortedListWithTrackNumbers.append((item[0], item[1], item[2], item[3], audioFile["tracknumber"][0]))
-        trackNumber += 1
+    sorted_list_with_tracknumber = []
+    track_number = 1
+    for item in sorted_list:
+        audio_file = EasyID3(item[0])
+        audio_file["tracknumber"] = [str(track_number)]
+        audio_file.save(item[0], v1=2, v2_version = 3)
+        sorted_list_with_tracknumber.append((item[0], item[1], item[2], item[3], audio_file["tracknumber"][0]))
+        track_number += 1
 
-    return sortedListWithTrackNumbers
+    return sorted_list_with_tracknumber
 
 # Expects all items in the list to have an ID3 tag with atleast: title, album, and artist
-def writeOutput(sortedList):
+def write_output(sorted_list):
 
     # encoding="utf-8" accounts for text written in other languages
     with io.open("Music Menu.txt", "w", encoding="utf-8") as output:
 
         # Get padding size for neat formating
-        colWidth1 = 1
-        colWidth2 = 1
-        colWidth4 = 1
-        for item in sortedList:
-            if len(item[1]) > colWidth1:
-                colWidth1 = len(item[1])
-            if len(item[2]) > colWidth2:
-                colWidth2 = len(item[2])
-            if len(item[4]) > colWidth4:
-                colWidth4 = len(item[4])
+        col_width1 = 1
+        col_width2 = 1
+        col_width4 = 1
+        for item in sorted_list:
+            if len(item[1]) > col_width1:
+                col_width1 = len(item[1])
+            if len(item[2]) > col_width2:
+                col_width2 = len(item[2])
+            if len(item[4]) > col_width4:
+                col_width4 = len(item[4])
         # Padding
-        colWidth1 += -94    # For lengthy titles
-        colWidth2 += -42
-        colWidth4 += 2
+        col_width1 += -94    # For lengthy titles
+        col_width2 += -42
+        col_width4 += 2
 
-        for item in sortedList:
-            output.write(item[4].ljust(colWidth4) + ' ' + item[1].ljust(colWidth1, '.') + ' ' + ''.ljust(1, '\t') + ' ' + item[2].ljust(colWidth2, '.') + ' ' + ''.ljust(1, '\t') + ' ' + item[3] + '\n')
+        for item in sorted_list:
+            output.write(item[4].ljust(col_width4) + ' ' + item[1].ljust(col_width1, '.') + ' ' + ''.ljust(1, '\t') + ' ' + item[2].ljust(col_width2, '.') + ' ' + ''.ljust(1, '\t') + ' ' + item[3] + '\n')
 
-def transferFiles(sortedList, path):
-    for item in sortedList:
-        properPath = path.replace('\\', "\\\\")
+# Converts user input raw_path into unicode_path
+# Transfer files one by one
+def transfer_files(sorted_list, raw_path):
+    for item in sorted_list:
+        unicode_path = raw_path.replace('\\', "\\\\")
         print("Copying " + item[0] + "...")
-        copy2(item[0], properPath)
+        copy2(item[0], unicode_path)
 
 def main():
     # Prepare for transfer
     print("Acquiring MP3 files within the directory...\n")
-    mp3Files = GetCurrentDirectoryMp3List()
+    mp3_files = get_current_directory_mp3()
     print("Sorting MP3 files within the directory...\n")
-    sortedMp3Files = getSortedList(mp3Files)
+    sorted_mp3_files = get_sorted_list(mp3_files)
     print("Creating list of sorted MP3 files within the directory: \"Music Menu\"\n")
-    writeOutput(sortedMp3Files)
+    write_output(sorted_mp3_files)
     
     # Transfer mp3 files to a specified directory
     path = input("Enter the path to transfer the music to: ")
     print("Transfering music to: ", path)
-    transferFiles(sortedMp3Files, path)
+    transfer_files(sorted_mp3_files, path)
 
     print("-- TRANSFER COMPLETE --\n")
 
