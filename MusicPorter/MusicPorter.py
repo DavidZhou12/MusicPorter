84465b5898301a54056c85ba6d665eb98ff2716b
David Zhou <DavidZhouToday@gmail.com>
2017-05-27 20:32:23 -0400

    Add multiple level sorting to getSortedList method

--- MusicPorter/MusicPorter.py
+++ MusicPorter/MusicPorter.py
@@ -2,6 +2,10 @@ import mutagen
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3, TIT2
 import io, os, sys
+from operator import itemgetter, attrgetter
+
+# Items in the mp3List are in tuples with the following order:
+# (mp3FilePath, Title, Album, Artist, Track Number)
 
 def printEditableList():
     print('\n'.join(EasyID3.valid_keys.keys()))
@@ -16,7 +20,7 @@ def GetCurrentDirectoryMp3List():
     mp3Files = [file for file in os.listdir() if isMp3(file)]
     return mp3Files
 
-# Ensures every mp3 file in the list has an ID3 tag
+# Ensures every mp3 file in the list has an ID3 tag with: title, album, artist
 # Returns a sorted list of every mp3 file
 def getSortedList(mp3Files):
     sortedList = []
@@ -43,20 +47,20 @@ def getSortedList(mp3Files):
             audioFile["album"] = ["Unknown Album"]
             audioFile["artist"] = ["Unknown Artist"]
             audioFile.save(mp3FilePath, v1=2, v2_version = 3)
-        sortedList.append((audioFile['title'][0], audioFile['album'][0], audioFile['artist'][0]))
+
+        sortedList.append((mp3FilePath, audioFile['title'][0], audioFile['album'][0], audioFile['artist'][0]))
+        sortedList = sorted(sortedList, key=itemgetter(2, 3, 1, 0))
+        
+        # Except every item to have an ID3 tag at this point
+
     return sortedList
 
-def writeOutput(mp3Files):
+# Expects all items in the list to have an ID3 tag with atleast: title, album, and artist
+def writeOutput(sortedList):
+
+    # encoding="utf-8" accounts for text written in other languages
     with io.open("Output.txt", "w", encoding="utf-8") as output:
-        for mp3File in mp3Files:
-            try:                                    # Test if file has an ID3 tag
-                audioFile = EasyID3(mp3File)
-            except mutagen.id3.ID3NoHeaderError:    # if it doesn't have an ID3 tag, create an ID3 tag for it
-                audioFile = EasyID3()
-                audioFile["title"] = [os.path.splitext(mp3File)[0]]
-                audioFile["album"] = ["Unknown Album"]
-                audioFile['artist'] = ["Unknown Artist"]
-                audioFile.save(mp3File, v1=2, v2_version = 3)
+        for mp3File in sortedList:
             output.write(audioFile['title'][0] + ' ' + audioFile['album'][0] + ' ' + audioFile['artist'][0] + '\n')
 
 def main():
